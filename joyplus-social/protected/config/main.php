3c24adaff46479f1205cd6a811e71d7abe2469dc
Dale2012 <long781224@hotmail.com>
2012-09-24 18:23:31 +0800

    d

--- joyplus-social/protected/config/main.php
+++ joyplus-social/protected/config/main.php
@@ -100,9 +100,9 @@ return array(
 				),
 				// uncomment the following to show log messages on web pages
 				
-				array(
-					'class'=>'CWebLogRoute',
-				),
+				//array(
+				//	'class'=>'CWebLogRoute',
+				//),
 				
 			),
 		),
