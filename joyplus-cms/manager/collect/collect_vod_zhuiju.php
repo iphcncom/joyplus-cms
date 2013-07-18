ce95e263b655f1163bca5f15b294a576f2fec3da
刘洋 <yang.liu@joyplus.tv>
2013-07-18 13:46:16 +0800

    ly

--- joyplus-cms/manager/collect/collect_vod_zhuiju.php
+++ joyplus-cms/manager/collect/collect_vod_zhuiju.php
@@ -215,7 +215,7 @@ $(document).ready(function(){
 	<td colspan="4">
     <input name="chkall" type="checkbox" id="chkall" value="1" onClick="checkAll(this.checked,'m_id[]');"/> 全选
      &nbsp;<input type="button" id="btnDel" value="批量删除" class="btn"  />
-     &nbsp;<input type="button" id="btnDelall" value="删除所有" class="btn"  />
+     
      &nbsp;<input type="button" value="批量修改" id="btnEdit" class="input" />
 	</td>
 	</tr>
